2e025b660222d7c0fc2fd8dba3407b40b3e007cc
Umut Cakar <umut.cakar@polisoft.com.tr>
2019-05-22 13:38:22 +0300

    fix

--- djangotodoapp/djangotodoapp/urls.py
+++ djangotodoapp/djangotodoapp/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('addTodo', views.addTodo),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.delete),
 ]
